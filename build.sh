a8ce3e26b97928c20640094dc827024e75d61557
artas182x <artas182x@gmail.com>
2015-01-12 22:13:39 +0100

    Add fsync control

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ make clean
 echo "Preparing for build"
 make exynos5260-hlltexx_00_defconfig ARCH=arm
 echo "Starting building.."
-ccache make -j6 ARCH=arm CROSS_COMPILE=/home/artur/arm-eabi-4.6/bin/arm-eabi- zImage
+ccache make -j6 ARCH=arm CROSS_COMPILE=/home/artur/arm-eabi-4.8/bin/arm-eabi- zImage
