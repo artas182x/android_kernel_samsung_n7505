9633862ae463e24119863369f09193cad6dbb7c5
artas182x <artas182x@gmail.com>
2015-01-16 20:11:03 +0100

    Disable governors

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ make clean
 echo "Preparing for build"
 make exynos5260-hlltexx_00_defconfig ARCH=arm
 echo "Starting building.."
-ccache make -j6 ARCH=arm CROSS_COMPILE=/home/artur/linaro_toolchains_2014/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2014.12/bin/arm-cortex_a15-linux-gnueabihf- zImage
+ccache make -j6 ARCH=arm CROSS_COMPILE=/home/artur/arm-eabi-4.8/bin/arm-eabi- zImage
